b4188dc5e1b05e854d63f5128e3311d376c670b3
rkaunismaa <rkaunis@gmail.com>
2023-07-21 11:02:50 -0400

    sort the output list

---
+++ newegg/sortList.py
@@ -0,0 +1,20 @@
+import json
+
+dir = 'newegg/'
+ext = '.jsonl'
+source = 'newegg' 
+target = dir + source + '_sorted' + ext
+
+# Step 1: Read the JSONL file into a list
+with open(dir + source + ext, 'r') as file:
+    lines = file.readlines()
+
+# Step 2: Parse each line as JSON and sort the list based on the "name" key
+data_list = [json.loads(line) for line in lines]
+sorted_data_list = sorted(data_list, key=lambda x: (x['sourceUrl'], x['tagmedal']))
+
+
+# Step 3: Write the sorted list back to the JSONL file
+with open(target, 'w') as file:
+    for item in sorted_data_list:
+        file.write(json.dumps(item) + '\n')
