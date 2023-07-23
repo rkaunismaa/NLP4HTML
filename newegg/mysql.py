0ff43edb6dfb8cbc894703d20842b830a5ff69c0
rkaunismaa <rkaunis@gmail.com>
2023-07-23 17:54:12 -0400

    why cant I connect to mysql??

--- newegg/mysql.py
+++ newegg/mysql.py
@@ -1,15 +1,22 @@
-import mysql.connector
+import sys
+print("/////////////")
+print(sys.path)
 
-# Replace these with your actual database credentials
-db_host = "172.17.0.3"
-db_user = "root"
-db_password = "12345"
-db_name = "NewEggCa"
 
-# Establish a connection
-conn = mysql.connector.connect(
-    host=db_host,
-    user=db_user,
-    password=db_password,
-    database=db_name
-)
+
+# # from mysql.connector import *
+# import mysql.connector
+
+# # Replace these with your actual database credentials
+# db_host = "172.17.0.2"
+# db_user = "root"
+# db_password = "12345"
+# db_name = "NewEggCa"
+
+# # Establish a connection
+# conn = mysql.connector.connect(
+#     host=db_host,
+#     user=db_user,
+#     password=db_password,
+#     database=db_name
+# )
