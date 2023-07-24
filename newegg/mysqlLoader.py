b01dc6b1bead616c866686d9e0eb5f09905677a2
rkaunismaa <rkaunis@gmail.com>
2023-07-24 12:12:54 -0400

    Nice! It all inserts into MySql!

--- newegg/mysqlLoader.py
+++ newegg/mysqlLoader.py
@@ -25,17 +25,16 @@ source = 'newegg'
 
 sourceFile = dir + source + ext
 
-# # Step 1: Read the JSONL file into a list
-# with open(sourceFile, 'r') as file:
-#     lines = file.readlines()
+with conn.cursor() as cursor:
+    query = f'DELETE FROM {table_name}'
+    cursor.execute(query)
+conn.commit()
 
-# # Step 2: Parse each line as JSON and sort the list based on the "name" key
-# data_list = [json.loads(line) for line in lines]
+with conn.cursor() as cursor:
+    query = f'ALTER TABLE {table_name} AUTO_INCREMENT=1'
+    cursor.execute(query)
+conn.commit()
 
-# line0 = ast.literal_eval(lines[0])
-
-# for key, value in line0.items():
-#     print(key, value)
 
 # Open the file and read JSON objects line by line
 with open(sourceFile, "r") as file:
@@ -44,11 +43,20 @@ with open(sourceFile, "r") as file:
         # Parse the JSON object in each line
         product = json.loads(line)
 
-        with conn.cursor() as cursor:
-            for key, value in product.items():
-                if key in ['sourceUrl', 'tagmedal', 'title', 'url', 'imgsrc', 'price', 'wasprice', 'savings']:
-                    insertQuery = f'INSERT INTO {table_name} ({key}) VALUES (%s)'
-                    shortValue = value[0:10]
-                    cursor.execute(insertQuery,(shortValue,))
+        keys = []
+        values = []
+        for key, value in product.items():
+            if key in ['dateTime', 'sourceUrl', 'tagmedal', 'title', 'url', 'imgsrc', 'price', 'wasprice', 'savings', 'bullets']:
+                keys.append(key)
+                if key == 'bullets':
+                    value = ",".join(value)
+                values.append(value)
 
+        keyS = ",".join(keys)
+        valueS = tuple(values) 
+        with conn.cursor() as cursor:
+            insertQuery = f'INSERT INTO {table_name} ({keyS}) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+            cursor.execute(insertQuery, valueS)
+
+conn.commit()
 conn.close()
