2ef6623f1d2b1f0a7a3bd5654fb8a2430feae768
rkaunismaa <rkaunis@gmail.com>
2023-07-23 20:39:09 -0400

    working on saving stuff to products

--- newegg/mysqlLoader.py
+++ newegg/mysqlLoader.py
@@ -6,6 +6,7 @@ db_host = "172.17.0.2"
 db_user = "root"
 db_password = "12345"
 db_name = "NewEggCa"
+table_name = "Products"
 
 # Establish a connection
 conn = mysql.connector.connect(
@@ -16,19 +17,38 @@ conn = mysql.connector.connect(
 )
 
 import json
+import ast
 
 dir = 'newegg/'
 ext = '.jsonl'
 source = 'newegg' 
-target = dir + source + '_sorted' + ext
 
-# Step 1: Read the JSONL file into a list
-with open(dir + source + ext, 'r') as file:
-    lines = file.readlines()
+sourceFile = dir + source + ext
 
-# Step 2: Parse each line as JSON and sort the list based on the "name" key
-data_list = [json.loads(line) for line in lines]
+# # Step 1: Read the JSONL file into a list
+# with open(sourceFile, 'r') as file:
+#     lines = file.readlines()
 
+# # Step 2: Parse each line as JSON and sort the list based on the "name" key
+# data_list = [json.loads(line) for line in lines]
 
+# line0 = ast.literal_eval(lines[0])
+
+# for key, value in line0.items():
+#     print(key, value)
+
+# Open the file and read JSON objects line by line
+with open(sourceFile, "r") as file:
+    for line in file:
+
+        # Parse the JSON object in each line
+        product = json.loads(line)
+
+        with conn.cursor() as cursor:
+            for key, value in product.items():
+                if key in ['sourceUrl', 'tagmedal', 'title', 'url', 'imgsrc', 'price', 'wasprice', 'savings']:
+                    insertQuery = f'INSERT INTO {table_name} ({key}) VALUES (%s)'
+                    shortValue = value[0:10]
+                    cursor.execute(insertQuery,(shortValue,))
 
 conn.close()
