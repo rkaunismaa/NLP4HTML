b91aa309f1a44adc20a35f2f08d1b11435c8f5a2
rkaunismaa <rkaunis@gmail.com>
2023-07-27 18:44:19 -0400

    seem to get a bit further, but still problems with seleium

--- TestSelenium/test_first_script.py
+++ TestSelenium/test_first_script.py
@@ -2,23 +2,36 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.options import Options
 
 def test_eight_components():
 
-    # driver = webdriver.Chrome()
+
+    # options = Options()
+    # options.binary_location = '/opt/google/chrome/chrome'
+
+
+    # /opt/google/chrome/chrome
+
+    # driver = webdriver.Chrome(exe)
+
+   #  driver = webdriver.Chrome(options=options)
+
+
     # PATH_TO_GECKO_DRIVER = '/usr/local/bin'
     PATH_TO_GECKO_DRIVER = '/home/rob/Data/Documents/SeleniumDrivers'
-   # driver = webdriver.Firefox(executable_path=PATH_TO_GECKO_DRIVER)
+    
+    driver = webdriver.Firefox(executable_path='/usr/bin/firefox')
 
     # Let's try Brave with the chromium driver ...
     PATH_TO_BRAVE = '/snap/bin/brave'
     PATH_TO_CHROMIUM_DRIVER = '/usr/local/bin'
 
-    driver = webdriver.Chrome(executable_path=PATH_TO_GECKO_DRIVER)
+   #  driver = webdriver.Chrome(executable_path=PATH_TO_GECKO_DRIVER)
 
     # Replace 'PATH_TO_CHROMEDRIVER' with the actual path to the chromedriver executable
-    brave_options = webdriver.ChromeOptions()
-    brave_options.binary_location = PATH_TO_BRAVE  # Replace with the actual path to Brave browser binary
+    # brave_options = webdriver.ChromeOptions()
+    # brave_options.binary_location = PATH_TO_BRAVE  # Replace with the actual path to Brave browser binary
 
     # driver = webdriver.Chrome(executable_path=PATH_TO_CHROMIUM_DRIVER, options=brave_options)
 
