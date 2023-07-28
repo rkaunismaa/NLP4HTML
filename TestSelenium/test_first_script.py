85491521a10435c795a261d0197b2618f95a13c9
rkaunismaa <rkaunis@gmail.com>
2023-07-28 17:07:22 -0400

    Nice! I can now scrape match using selenium!

--- TestSelenium/test_first_script.py
+++ TestSelenium/test_first_script.py
@@ -13,10 +13,13 @@ from selenium.webdriver.common.by import By
 def test_eight_components():
 
 
-    useChrome = False
+    useChrome = True
     useFireFox = not useChrome
 
     # IF this fails, then try killing any running instances of brave, then try again ... 
+    # launch brave from the terminal window using the following command ...
+    # brave --remote-debugging-port=9224
+    # This needs to be done BEFORE you fire this script! ... 
     if useChrome:
         chromeOptions = ChromeOptions()
         chromeOptions.headless = True
@@ -31,24 +34,43 @@ def test_eight_components():
        # firefoxOptions.binary_location = '/snap/bin/firefox'
         driver = webdriver.Firefox(options=firefoxOptions)
 
-    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
 
-    title = driver.title
-    assert title == "Web form"
+    testSeleniumWebForm = False
+    if testSeleniumWebForm :
 
-    driver.implicitly_wait(0.5)
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html")
 
-    text_box = driver.find_element(by=By.NAME, value="my-text")
-    submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+        title = driver.title
+        assert title == "Web form"
 
-    text_box.send_keys("Selenium")
-    submit_button.click()
+        driver.implicitly_wait(0.5)
 
-    message = driver.find_element(by=By.ID, value="message")
-    value = message.text
-    assert value == "Received!"
-    
-    print("Success!!")
+        text_box = driver.find_element(by=By.NAME, value="my-text")
+        submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+
+        text_box.send_keys("Selenium")
+        submit_button.click()
+
+        message = driver.find_element(by=By.ID, value="message")
+        value = message.text
+        assert value == "Received!"
+        
+        print("Selenium Web Form Success!!")
+
+
+    # So let's try match, shall we ... 
+    if useChrome:
+
+        driver.get("https://www.match.com/profile/me/edit")
+
+        userName = driver.find_element(By.XPATH, '//*[@id="profile-edit-layout"]/div[2]/div[2]/div/section/div/div[1]/div/div[1]/div[1]/h2')
+        myName = userName.text
+        assert myName == "Rob"
+
+        print("Match Success!")
+
+
+
 
     driver.quit()
 
