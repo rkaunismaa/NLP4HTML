49de3a48b635fa3d967352b2ff656c15f7f3ce44
rkaunismaa <rkaunis@gmail.com>
2023-07-29 17:21:46 -0400

    I was able to easily download all the images! Nice!

--- TestSelenium/Match2MySql.py
+++ TestSelenium/Match2MySql.py
@@ -1,4 +1,6 @@
 import pickle
+import requests
+import shutil
 
 # Save the userProfiles to a local file
 fileName = 'matchProfiles.txt'
@@ -6,4 +8,24 @@ fileName = 'matchProfiles.txt'
 with open(fileName, "rb") as input_file:
     userProfiles = pickle.load(input_file)
 
-print(userProfiles)
+
+for user in userProfiles:
+
+    imageList = user[3]
+
+    for image in imageList:
+
+        url = image[0]
+        urlParts = url.split("/")
+        iName = urlParts[-1]
+
+        res = requests.get(url, stream = True)
+
+        if res.status_code == 200:
+            with open(iName,'wb') as f:
+                shutil.copyfileobj(res.raw, f)
+            print('Image sucessfully Downloaded: ',iName)
+        else:
+            print('Image Couldn\'t be retrieved')
+
+
