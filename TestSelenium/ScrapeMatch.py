b41817814685bbd3b27c99ba3e1b5eecc96e0f94
rkaunismaa <rkaunis@gmail.com>
2023-07-31 12:02:02 -0400

    more tweaking and scanning of match

--- TestSelenium/ScrapeMatch.py
+++ TestSelenium/ScrapeMatch.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
-from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, StaleElementReferenceException
 
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
@@ -47,51 +47,50 @@ generateNewList = True
 if generateNewList:
 
     userList = []
+    profileIDS = []
 
     searchPage = "https://www.match.com/search"
     searchPage = "https://www.match.com/search/mutual"
     searchPage = "https://www.match.com/search?sortBy=11"
    
     driver.get(searchPage)
-
     driver.implicitly_wait(5)
 
-    # load a few more profiles ...
-  #  driver.execute_script("window.scrollBy(0,4000)","")
+    def scanPage():
 
-    # driver.execute_script("window.scrollBy(0,2000)","")
+        # select all the "css-1trsig2" css selectors 
+        profiles = driver.find_elements(By.CLASS_NAME, "css-1trsig2")
 
-    # driver.execute_script("window.scrollBy(0,2000)","")
+        for profile in profiles:
 
-    # driver.execute_script("window.scrollBy(0,2000)","")
+            try:
 
-    # driver.execute_script("window.scrollBy(0,2000)","")
+                url = profile.get_attribute("href")
+                urlParts = url.split("?")
+                profilePage =urlParts[0]
 
-    # driver.implicitly_wait(5)
+                profilePageParts = profilePage.split("/")
+                profileId = profilePageParts[-1]
 
-    # select all the "css-1trsig2" css selectors 
-    profiles = driver.find_elements(By.CLASS_NAME, "css-1trsig2")
+                personName = profile.find_element(By.CLASS_NAME, "css-1jab1x0").text
+                personAgeLocation = profile.find_element(By.CLASS_NAME, "css-3g75q9").text
+                personAgeLocationHidden = profile.find_element(By.CLASS_NAME, "css-17ertmd").text
 
-    for profile in profiles:
+                if not profileId in profileIDS:
+                    userList.append( (personName, profilePage, profileId, personAgeLocation, personAgeLocationHidden) )
+                    profileIDS.append(profileId)
 
-        try:
+            except NoSuchElementException:
+                print('NoSuchElementException!')
 
-            url = profile.get_attribute("href")
-            urlParts = url.split("?")
-            profilePage =urlParts[0]
+            except Exception:
+                print('Exception!')
 
-            personName = profile.find_element(By.CLASS_NAME, "css-1jab1x0").text
-            personAgeLocation = profile.find_element(By.CLASS_NAME, "css-3g75q9").text
-            personAgeLocationHidden = profile.find_element(By.CLASS_NAME, "css-17ertmd").text
 
-            userList.append( (personName, profilePage, personAgeLocation, personAgeLocationHidden) )
 
-        except NoSuchElementException:
-            print('NoSuchElementException!')
-
-        except Exception:
-            print('Exception!')
-
+    for _ in range(5):
+        scanPage()
+        driver.execute_script("window.scrollBy(0,2000)","")
 
     # Let's save this list, shall we ...
     # Save the userProfiles to a local file
@@ -101,37 +100,20 @@ if generateNewList:
 
     print(len(userList))
 
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
 # This will store all the metadata we want on the user.
 userProfiles = []
 
-for testUser in userList:
 
-    profilePage = testUser[1]
-    driver.get(profilePage)
+def scanProfilePage_():
 
-    # Set an implicit wait of 10 seconds
-    time.sleep(5)
-    
-    # Set an explicit wait of 5 seconds for the carousel button to be clickable
+ 
+    # Set an explicit wait of 10 seconds for the carousel button to be clickable
     carouselButtonXPATH = '//*[@id="mainContent"]/article/div[2]/div[1]/div[2]/div/button'
-    wait = WebDriverWait(driver, 5)
-    element = wait.until(EC.element_to_be_clickable((By.XPATH, carouselButtonXPATH)))
+    try:
+        wait = WebDriverWait(driver, 10)
+        photo_carousel_button = wait.until(EC.element_to_be_clickable((By.XPATH, carouselButtonXPATH)))
+    except StaleElementReferenceException:
+        photo_carousel_button = driver.find_element(By.XPATH, carouselButtonXPATH)
 
     userName = driver.find_element(By.XPATH, '//*[@id="mainContent"]/article/div[2]/div[2]/div[1]/div[1]/div/h6')
     personName = userName.text
@@ -145,16 +127,19 @@ for testUser in userList:
     except NoSuchElementException:
         personSummary = ""
 
-    photo_carousel_button = driver.find_element(By.XPATH, carouselButtonXPATH)
+  #   photo_carousel_button = driver.find_element(By.XPATH, carouselButtonXPATH)
     photo_carousel_button.click()
 
     # Set an implicit wait of 5 seconds
-    driver.implicitly_wait(5)
+    # driver.implicitly_wait(5)
 
       # Set an explicit wait of 5 seconds for the carousel button to be clickable
     carouselCloseButtonXPATH = '//*[@id="modalHeader"]/button/span'
-    wait = WebDriverWait(driver, 5)
-    element = wait.until(EC.element_to_be_clickable((By.XPATH, carouselCloseButtonXPATH)))
+    try:
+        wait = WebDriverWait(driver, 5)
+        photo_carousel_close_button = wait.until(EC.element_to_be_clickable((By.XPATH, carouselCloseButtonXPATH)))
+    except StaleElementReferenceException:
+        photo_carousel_close_button = driver.find_element(By.XPATH, carouselCloseButtonXPATH)
 
     image_count = driver.find_element(By.XPATH, '//*[@id="lightbox-image-caption"]/span')
     no_of_images = image_count.text # '1/5'
@@ -184,13 +169,87 @@ for testUser in userList:
         if nextImageButton:
             photo_carousel_right_arrow_button.click() 
             # Set an implicit wait of 2 seconds
-            driver.implicitly_wait(2)   
+            # driver.implicitly_wait(2)   
 
     # close the photo carousel
-    photo_carousel_close_button = driver.find_element(By.XPATH, carouselCloseButtonXPATH)
+   #  photo_carousel_close_button = driver.find_element(By.XPATH, carouselCloseButtonXPATH)
     photo_carousel_close_button.click()
 
     userProfiles.append( (profilePage, personName, personAgeLocation, personSummary, imageList) )
+
+
+def scanProfilePage():
+
+    try: 
+
+        # Set an explicit wait of 10 seconds for the carousel button to be clickable
+        carouselButtonXPATH = '//*[@id="mainContent"]/article/div[2]/div[1]/div[2]/div/button'
+        photo_carousel_button = driver.find_element(By.XPATH, carouselButtonXPATH)
+
+        userName = driver.find_element(By.XPATH, '//*[@id="mainContent"]/article/div[2]/div[2]/div[1]/div[1]/div/h6')
+        personName = userName.text
+
+        ageLocation = driver.find_element(By.XPATH, '//*[@id="mainContent"]/article/div[2]/div[2]/div[2]/span')
+        personAgeLocation = ageLocation.text
+
+        try:
+            summary = driver.find_element(By.XPATH, '//*[@id="mainContent"]/article/section[1]/div/div/div/div[1]/span')    
+            personSummary = summary.text    
+        except NoSuchElementException:
+            personSummary = ""
+
+        photo_carousel_button.click()
+
+        # Set an explicit wait of 5 seconds for the carousel button to be clickable
+        carouselCloseButtonXPATH = '//*[@id="modalHeader"]/button/span'
+        photo_carousel_close_button = driver.find_element(By.XPATH, carouselCloseButtonXPATH)
+
+        image_count = driver.find_element(By.XPATH, '//*[@id="lightbox-image-caption"]/span')
+        no_of_images = image_count.text # '1/5'
+        noiList = no_of_images.split('/')
+        totalImages = int(noiList[1])
+
+        # user may only have one image, so no right button
+        try:
+            photo_carousel_right_arrow_button = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/section/div/button[2]')
+            nextImageButton = True
+        except NoSuchElementException:                          
+            nextImageButton = False
+
+        photo_carousel_image = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/section/div/figure/div[2]/button/img')
+
+        imageList = []
+        for _ in range(totalImages):
+
+            image_src = photo_carousel_image.get_attribute("src")
+            try:
+                photo_carousel_caption = driver.find_element(By.XPATH, '//*[@id="lightbox-image-caption"]/div/span')
+                image_caption = photo_carousel_caption.text
+            except NoSuchElementException:
+                image_caption = ""
+
+            imageList.append( (image_src, image_caption) )
+            if nextImageButton:
+                photo_carousel_right_arrow_button.click() 
+
+        photo_carousel_close_button.click()
+
+        userProfiles.append( (profilePage, personName, personAgeLocation, personSummary, imageList) )
+
+        print(f'Page scan for {personName} Success!')
+
+    except Exception:
+        # something failed ... move on ..
+        print("Page scan failure! ... move on!")
+
+
+for testUser in userList:
+
+    profilePage = testUser[1]
+    driver.get(profilePage)
+
+    scanProfilePage()
+   
 
 # Save the userProfiles to a local file
 fileName = 'matchProfiles.txt'
