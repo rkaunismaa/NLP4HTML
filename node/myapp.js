cb9940e6a17e5bcae922437a6764c863b9419737
rkaunismaa <rkaunis@gmail.com>
2023-08-02 16:17:48 -0400

    working through the Node.js tutorial found at W3Schools

--- node/myapp.js
+++ node/myapp.js
@@ -1,12 +1,19 @@
-const http = require('http');
+const http = require("http");
 
-const hostname = '127.0.0.1';
+var dt = require("./myfirstmodule");
+
+const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  // res.statusCode = 200;
+  // res.setHeader('Content-Type', 'text/plain');
+
+  res.writeHead(200, { "Content-Type": "text/html" });
+
+  res.write("The date and time are currently => " + dt.myDateTime());
+
+  res.end();
 });
 
 server.listen(port, hostname, () => {
