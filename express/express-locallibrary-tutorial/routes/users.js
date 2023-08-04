282337f46f64e7efad782e60153247741354f7e3
rkaunismaa <rkaunis@gmail.com>
2023-08-04 14:00:12 -0400

    add the /users/cool route

--- express/express-locallibrary-tutorial/routes/users.js
+++ express/express-locallibrary-tutorial/routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET users listing. */
+router.get('/cool', function(req, res, next) {
+  res.send('Youre so cool!');
+});
+
 module.exports = router;
