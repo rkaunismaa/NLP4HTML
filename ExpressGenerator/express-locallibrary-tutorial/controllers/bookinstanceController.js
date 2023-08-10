73a1da236c06f6d8c9673814c5079f4d6b9aeaa2
rkaunismaa <rkaunis@gmail.com>
2023-08-10 17:06:06 -0400

    wireup the book instance list page

--- ExpressGenerator/express-locallibrary-tutorial/controllers/bookinstanceController.js
+++ ExpressGenerator/express-locallibrary-tutorial/controllers/bookinstanceController.js
@@ -2,8 +2,16 @@ const BookInstance = require("../models/bookinstance");
 const asyncHandler = require("express-async-handler");
 
 // Display list of all BookInstances.
+// exports.bookinstance_list = asyncHandler(async (req, res, next) => {
+//   res.send("NOT IMPLEMENTED: BookInstance list");
+// });
 exports.bookinstance_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance list");
+  const allBookInstances = await BookInstance.find().populate("book").exec();
+
+  res.render("bookinstance_list", {
+    title: "Book Instance List",
+    bookinstance_list: allBookInstances,
+  });
 });
 
 // Display detail page for a specific BookInstance.
