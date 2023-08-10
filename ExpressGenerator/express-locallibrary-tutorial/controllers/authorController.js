4ab48e03f3e4c3780de9601fe928b4fb1d2ecbd9
rkaunismaa <rkaunis@gmail.com>
2023-08-10 17:29:52 -0400

    more tweaks to author list page

--- ExpressGenerator/express-locallibrary-tutorial/controllers/authorController.js
+++ ExpressGenerator/express-locallibrary-tutorial/controllers/authorController.js
@@ -5,6 +5,15 @@ const asyncHandler = require("express-async-handler");
 // exports.author_list = asyncHandler(async (req, res, next) => {
 //   res.send("NOT IMPLEMENTED: Author list");
 // });
+
+// exports.author_list = asyncHandler(async (req, res, next) => {
+//   const allAuthors = await Author.find().sort({ family_name: 1 }).exec();
+//   res.render("author_list", {
+//     title: "Author List",
+//     author_list: allAuthors,
+//   });
+// });
+
 exports.author_list = asyncHandler(async (req, res, next) => {
   const allAuthors = await Author.find().sort({ family_name: 1 }).exec();
   res.render("author_list", {
