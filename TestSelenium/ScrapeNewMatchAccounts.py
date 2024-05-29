80e7d0fbe269695fb15ee59dfa37976d28998ec4
rkaunismaa <rkaunis@gmail.com>
2024-05-29 18:39:45 -0400

    looks like they are on to me!

--- TestSelenium/ScrapeNewMatchAccounts.py
+++ TestSelenium/ScrapeNewMatchAccounts.py
@@ -14,50 +14,11 @@ from datetime import date
 
 from MatchFunctions import *
 
-# Master Profile list ... always specify the most recent master list last!
-fnProfileMaster = 'matchLists/MatchProfilesMasterList.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2023-08-30--10-25.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2023-09-03--12-21.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2023-09-06--16-14.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2023-09-13--09-49.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2023-09-18--10-20.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2023-09-25--16-27.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2023-10-06--07-12.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2023-10-15--11-39.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2023-10-22--06-53.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2023-11-02--05-56.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2023-11-09--10-18.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2023-11-16--17-00.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2023-11-23--06-22.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2023-11-29--13-57.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2023-12-06--10-17.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2023-12-10--14-29.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2023-12-14--07-42.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2023-12-21--17-07.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2023-12-28--08-11.txt'
 
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2024-01-05--11-42.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2024-01-12--06-36.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2024-01-17--10-18.txt'
-
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2024-02-13--07-16.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2024-02-20--06-52.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2024-02-23--13-20.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2024-02-27--06-45.txt'
-
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2024-03-05--09-45.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2024-03-09--06-16.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2024-03-14--10-29.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2024-03-19--10-08.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2024-03-28--07-12.txt'
-
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2024-04-03--09-17.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2024-04-10--18-18.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2024-04-17--08-36.txt'
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2024-04-25--17-00.txt'
-
-fnProfileMaster = 'matchLists/MatchProfilesMasterList_2024-05-03--10-08.txt'
+# the old way ... 
 fnProfileMaster = 'matchLists/MatchProfilesMasterList_2024-05-08--08-09.txt'
+
+fnProfileMaster = get_newest_file_creation_date('matchLists', 'CurrentProfiles_')
 
 with open(fnProfileMaster, "rb") as input_file:
     profileMaster = pickle.load(input_file)
@@ -70,7 +31,8 @@ if (driver is not None) :
     todaysDate = date.today()
     startTime = time.time()
 
-    now = datetime.datetime.now()
+    # now = datetime.datetime.now()
+    now = datetime.now()
     yyymmdd_hhmm = now.strftime('%Y-%m-%d--%H-%M')
 
     userList = []
